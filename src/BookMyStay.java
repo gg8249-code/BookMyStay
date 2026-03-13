c361e2704e4d645d00f4d4660b8f0470032c9a96
gg8249-code <gg8249@srmist.edu.in>
2026-03-13 15:13:24 +0530

    UC1:Welcome message

---
+++ src/BookMyStay.java
@@ -0,0 +1,10 @@
+public class BookMyStay {
+
+    // Main method - Entry point of Java program
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully");
+
+    }
+}
